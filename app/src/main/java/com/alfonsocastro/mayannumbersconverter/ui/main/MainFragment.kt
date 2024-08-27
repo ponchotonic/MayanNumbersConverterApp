8779f7a221567ab24f8567ee8554f088504be18d
Alfonso Castro <ponchotonic@gmail.com>
2024-08-26 21:13:37 -0600

    Refactor Menu to MenuHost

--- app/src/main/java/com/alfonsocastro/mayannumbersconverter/ui/main/MainFragment.kt
+++ app/src/main/java/com/alfonsocastro/mayannumbersconverter/ui/main/MainFragment.kt
@@ -5,7 +5,10 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.alfonsocastro.mayannumbersconverter.R
@@ -26,11 +29,6 @@ class MainFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -46,67 +44,69 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.menu_main, menu)
-
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.share_action -> {
-                val sendIntent: Intent = Intent().apply {
-                    action = Intent.ACTION_SEND
-                    val message = String.format(getString(R.string.share_app_text), PLAY_STORE_LINK)
-                    putExtra(Intent.EXTRA_TEXT, message)
-                    type = "text/plain"
-                }
-
-
-                val shareIntent = Intent.createChooser(sendIntent, getString(R.string.share_app_title))
-                startActivity(shareIntent)
-                true
-            }
-            R.id.rate_action -> {
-                val reviewIntent: Intent = Intent().apply {
-                    action = Intent.ACTION_VIEW
-                    data = Uri.parse(PLAY_STORE_LINK)
-                }
-                startActivity(reviewIntent)
-                true
-            }
-            R.id.about_action -> {
-                val dialog = AlertDialog.Builder(requireContext())
-                    .setTitle(R.string.about_title)
-                    .setMessage(R.string.about_description)
-                    .setNeutralButton(R.string.about_contact_link) { _, _ ->
-                        val contactIntent: Intent = Intent().apply {
-                            action = Intent.ACTION_VIEW
-                            data = Uri.parse(TWITTER_PROFILE_LINK)
-                        }
-                        startActivity(contactIntent)
-                    }
-                    .setPositiveButton(android.R.string.ok, null)
-                    .create()
-                dialog.show()
-                true
-            }
-            R.id.privacy_policy_action -> {
-                val privacyIntent: Intent = Intent().apply {
-                    action = Intent.ACTION_VIEW
-                    data = Uri.parse(PRIVACY_POLICY_LINK)
-                }
-                startActivity(privacyIntent)
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_main, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.share_action -> {
+                        val sendIntent: Intent = Intent().apply {
+                            action = Intent.ACTION_SEND
+                            val message = String.format(getString(R.string.share_app_text), PLAY_STORE_LINK)
+                            putExtra(Intent.EXTRA_TEXT, message)
+                            type = "text/plain"
+                        }
+
+
+                        val shareIntent = Intent.createChooser(sendIntent, getString(R.string.share_app_title))
+                        startActivity(shareIntent)
+                        true
+                    }
+                    R.id.rate_action -> {
+                        val reviewIntent: Intent = Intent().apply {
+                            action = Intent.ACTION_VIEW
+                            data = Uri.parse(PLAY_STORE_LINK)
+                        }
+                        startActivity(reviewIntent)
+                        true
+                    }
+                    R.id.about_action -> {
+                        val dialog = AlertDialog.Builder(requireContext())
+                            .setTitle(R.string.about_title)
+                            .setMessage(R.string.about_description)
+                            .setNeutralButton(R.string.about_contact_link) { _, _ ->
+                                val contactIntent: Intent = Intent().apply {
+                                    action = Intent.ACTION_VIEW
+                                    data = Uri.parse(TWITTER_PROFILE_LINK)
+                                }
+                                startActivity(contactIntent)
+                            }
+                            .setPositiveButton(android.R.string.ok, null)
+                            .create()
+                        dialog.show()
+                        true
+                    }
+                    R.id.privacy_policy_action -> {
+                        val privacyIntent: Intent = Intent().apply {
+                            action = Intent.ACTION_VIEW
+                            data = Uri.parse(PRIVACY_POLICY_LINK)
+                        }
+                        startActivity(privacyIntent)
+                        true
+                    }
+                    else -> false
+                }
+            }
+            
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
     private fun displayAds() {
